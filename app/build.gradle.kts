37027b69b3fef9e22fbfbc495545c6f7575585ef
Xi Chen <52621857+xichen-de@users.noreply.github.com>
2026-08-29 18:38:18 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,8 @@ android {
 
     buildTypes {
         debug {
-            if (personalSigningConfigured) signingConfig = signingConfigs.getByName("personal")
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
         }
         release {
             isMinifyEnabled = true
